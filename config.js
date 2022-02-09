9399b1b8ba0033b60b1e3f5011f07be9ab7e21cc
HKM2005 <99192800+HKM2005@users.noreply.github.com>
2022-02-09 16:10:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Gửi crush của tớ",
-    introTitle: 'Crush yêu dấu',
+    titleWeb: "HKM2005 YÊU BÉ Ý NHIều :333!!!",
+    introTitle: ' Em bé yêu dấu của anh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Thoát web là chấp nhận yêu tớ rồi nhé!!!',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    desc: 'Thoát web là đồng ý làm ny tớ rồi nhé!!!',
+    btnYes: 'Thích lắm luôn cực kì nhiều luôn á <33',
+    btnNo: 'hong bé ơi :)))',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://m.me' //link mess của các bạn. VD: /dinhkaito
+    reply: 'Yêu cực kì nhiều luôn mà không dám nói :33',
+    mess: 'tui biết mà 🥰. Yêu bà nhiều nhiều lắm luôn đó 😘😘',
 }
